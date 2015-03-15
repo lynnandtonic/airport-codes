4e098fe8065c5e4b422a7494b42ffbf6bc3b5c6f
Lynn Fisher <lynnm.fisher@yahoo.com>
2015-03-14 21:49:40 -0700

    added BEY, fixes #53

--- data/index.js
+++ data/index.js
@@ -10,6 +10,7 @@ module.exports = {
     require('./aza.json'),
     require('./bdl.json'),
     require('./beg.json'),
+    require('./bey.json'),
     require('./bhm.json'),
     require('./bhx.json'),
     require('./bna.json'),
