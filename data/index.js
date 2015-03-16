d85f1f2fbeab5d84207aad41ae348434aabe6101
Lynn Fisher <lynnm.fisher@yahoo.com>
2015-03-15 20:13:59 -0700

    added YLT, #70

--- data/index.js
+++ data/index.js
@@ -159,6 +159,7 @@ module.exports = {
     require('./vie.json'),
     require('./waw.json'),
     require('./wlg.json'),
+    require('./ylt.json'),
     require('./ylw.json'),
     require('./yqb.json'),
     require('./yvr.json'),
