f08f5dfa1ef3a0292c4158e34174cfe1a76631d8
Lynn Fisher <lynnm.fisher@yahoo.com>
2015-03-19 07:53:17 -0700

    added RGN, fixes #133

--- data/index.js
+++ data/index.js
@@ -151,6 +151,7 @@ module.exports = {
     require('./pvd.json'),
     require('./pwm.json'),
     require('./rdu.json'),
+    require('./rgn.json'),
     require('./rix.json'),
     require('./roa.json'),
     require('./rsw.json'),
